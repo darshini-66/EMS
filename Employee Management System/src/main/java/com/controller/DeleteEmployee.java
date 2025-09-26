4a3640d50495f3526f1e3d7c27b6109841e40947
darshini-66 <darshinikantam@gmail.com>
2025-09-26 10:54:19 +0530

    first commit version1

---
+++ Employee Management System/src/main/java/com/controller/DeleteEmployee.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import java.util.List;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.EmployeeDAO;
+import com.model.Employee;
+
+
+@WebServlet("/delete")
+public class DeleteEmployee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		try {
+			EmployeeDAO dao = new EmployeeDAO();
+			boolean status= dao.deleteEmployee(id);
+			dao.commit();
+				RequestDispatcher dispatcher = request.getRequestDispatcher("findAll");
+	
+				dispatcher.forward(request, response);
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
